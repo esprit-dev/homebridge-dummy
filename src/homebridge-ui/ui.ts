8dab39a0c58910a90f12ed847ce3b4c5346eee01
mpatfield <martin@patfield.com>
2025-07-03 18:17:55 -0700

    Fix for missing crypto.randomUUID()

--- src/homebridge-ui/ui.ts
+++ src/homebridge-ui/ui.ts
@@ -75,13 +75,38 @@ const updateAccessoryNames = (strings: Translation) => {
   }
 };
 
+function generateUUID() {
+
+  if (typeof crypto !== 'undefined') {
+
+    if (crypto.randomUUID) {
+      return crypto.randomUUID();
+    }
+
+    if (crypto.getRandomValues) {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        const r = crypto.getRandomValues(new Uint8Array(1))[0] & 0xf;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      });
+    }
+
+  }
+
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 const updateConfigWithUUIDs = (config: DummyPlatformConfig) => {
 
   let changed = false;
 
   config.accessories?.forEach( (accessoryConfig) => {
     if (accessoryConfig.id === undefined) {
-      const id = crypto.randomUUID();
+      const id = generateUUID();
       accessoryConfig.id = id;
       changed = true;
     }
