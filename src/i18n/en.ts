3dbfe4365a1360552ee218215df4e2ed136cd50c
mpatfield <martin@patfield.com>
2025-07-16 14:32:45 -0700

    Adding hints for i18n replacements

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -2,53 +2,53 @@ const en = {
 
   accessory: {
 
-    missingRequired: '%s is missing required config variable %s',
+    missingRequired: '%s is missing required config variable %s', // accessory name, variable name
 
     command: {
-      executed: '%s executed command',
-      error: '%s failed to execute command',
+      executed: '%s executed command', // accessory name
+      error: '%s failed to execute command', // accessory name
     },
 
     lightbulb: {
-      brightness: '%s brightness is %s',
-      stateOn: '%s is on %s%',
+      brightness: '%s brightness is %s', // accessory name, number
+      stateOn: '%s is on %s%', // accessory name, number
     },
 
     lock: {
-      secured: '%s is locked',
-      unsecured: '%s is unlocked',
+      secured: '%s is locked', // accessory name
+      unsecured: '%s is unlocked', // accessory name
     },
 
     onOff: {
-      stateOn: '%s is on',
-      stateOff: '%s is off',
+      stateOn: '%s is on', // accessory name
+      stateOff: '%s is off', // accessory name
     },
 
     position: {
-      closed: '%s is closed',
-      open: '%s is open',
+      closed: '%s is closed', // accessory name
+      open: '%s is open', // accessory name
     },
 
     timer: {
-      cancel: 'Cancelled the timer for %s',
-      reset: 'Reset the timer for %s',
-      setSeconds: '%s is waiting %s seconds',
-      setMinutes: '%s is waiting %s minutes',
-      setHours: '%s is waiting %s hours',
+      cancel: 'Cancelled the timer for %s', // accessory name
+      reset: 'Reset the timer for %s', // accessory name
+      setSeconds: '%s is waiting %s seconds', // accessory name, number
+      setMinutes: '%s is waiting %s minutes',  // accessory name, number
+      setHours: '%s is waiting %s hours', // accessory name, number
     },
   },
 
   config: {
     migrate: 'Are you upgrading from an earlier version?',
-    migrationDetails1: 'HomeKit automations using %s accessories will need to be reconfigured!',
+    migrationDetails1: 'HomeKit automations using %s accessories will need to be reconfigured!', // plugin name
     migrationDetails2: 'Please downgrade to v0.9.2 or earlier if you want to keep your current setup.',
-    migrationDetails3: '%s can try to recreate your accessories, but you will still need reconfigure HomeKit automations.',
+    migrationDetails3: '%s can try to recreate your accessories, but you will still need reconfigure HomeKit automations.', // plugin name
     migrationDetails4: 'Would you like to recreate your accessories now?',
-    migrationDetails5: 'For more details, please visit %s',
+    migrationDetails5: 'For more details, please visit %s', // url
     migrationRestartTitle: 'Restart Homebridge',
-    migrationRestartDescription: 'Please restart Homebridge to begin %s migration',
-    support: 'For documentation and support please visit %s',
-    thankYou: 'Thank you for installing %s',
+    migrationRestartDescription: 'Please restart Homebridge to begin %s migration', // plugin name
+    support: 'For documentation and support please visit %s', // url
+    thankYou: 'Thank you for installing %s', // plugin name
     yes: 'Yes',
     no: 'No',
 
@@ -113,44 +113,44 @@ const en = {
   sensor: {
 
     carbonDioxide: {
-      active: '%s detected carbon dioxide',
-      inactive: '%s stopped detecting carbon dioxide',
+      active: '%s detected carbon dioxide', // accessory name
+      inactive: '%s stopped detecting carbon dioxide', // accessory name
     },
 
     carbonMonoxide: {
-      active: '%s detected carbon monoxide',
-      inactive: '%s stopped detecting carbon monoxide',
+      active: '%s detected carbon monoxide', // accessory name
+      inactive: '%s stopped detecting carbon monoxide', // accessory name
     },
 
     contact: {
-      active: '%s detected contact',
-      inactive: '%s stopped detecting contact',
+      active: '%s detected contact', // accessory name
+      inactive: '%s stopped detecting contact', // accessory name
     },
 
     leak: {
-      active: '%s detected a leak',
-      inactive: '%s stopped detecting leaks',
+      active: '%s detected a leak', // accessory name
+      inactive: '%s stopped detecting leaks', // accessory name
     },
 
     motion: {
-      active: '%s detected motion',
-      inactive: '%s stopped detecting motion',
+      active: '%s detected motion', // accessory name
+      inactive: '%s stopped detecting motion', // accessory name
     },
 
     occupancy: {
-      active: '%s detected occupancy',
-      inactive: '%s stopped detecting occupancy',
+      active: '%s detected occupancy', // accessory name
+      inactive: '%s stopped detecting occupancy', // accessory name
     },
 
     smoke: {
-      active: '%s detected smoke',
-      inactive: '%s stopped detecting smoke',
+      active: '%s detected smoke', // accessory name
+      inactive: '%s stopped detecting smoke', // accessory name
     },
   },
 
   startup: {
     migrationBridge: '‼️ Please restart Homebridge one more time for migration to take full effect ‼️',
-    migrationComplete: 'Successfully migrated %s accessories!',
+    migrationComplete: 'Successfully migrated %s accessories!', // number
     migrationNoAccessories: 'Unable to find any accessories to migrate',
     migrationIgnore: 'You may safely ignore \'No plugin was found…\' errors — they should go away the next time you restart Homebridge',
     migrationFailed: 'Sorry, something went wrong with the accessory migration',
@@ -159,7 +159,7 @@ const en = {
     removeAccessory: 'Removing accessory:',
     restoringAccessory: 'Restoring accessory:',
     setupComplete: '✓ Setup complete',
-    unsupportedType: 'Unsupported accessory type %s',
+    unsupportedType: 'Unsupported accessory type %s', // accessory type
     welcome: [
       'Please ★ this plugin on GitHub if you\'re finding it useful! https://github.com/mpatfield/homebridge-dummy',
       'Would you like to sponsor this plugin? https://github.com/sponsors/mpatfield',
